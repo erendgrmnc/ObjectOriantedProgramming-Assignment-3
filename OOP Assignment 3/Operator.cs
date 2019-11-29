4404faf6f2f91f18885cf53f76d9ad9d08ebec3c
erendgrmnc <erendgrmnc@users.noreply.github.com>
2019-11-29 14:49:35 +0300

    OOPAssignment3

---
+++ OOP Assignment 3/Operator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace ODEV_3_1._1
+{
+    class Operator
+    {
+        //Create ne dataTable
+        DataTable data_Operator = new DataTable();
+
+        protected string temp_Entry;
+        protected string result;
+
+        public string _restarter;
+        public bool restarter;
+
+        public void Calculate()
+        {
+            Console.WriteLine("Please Enter A Problem : ");
+
+            temp_Entry = Console.ReadLine();
+
+
+            //Uses DataTable's Compute function to Calculate Entered Problem.
+            result = Convert.ToString(data_Operator.Compute(temp_Entry.Trim(), null));
+
+            Console.WriteLine("The Result is :" + result);
+
+            Console.WriteLine("Do you want to calculate another problem ?(Press 'Y' if you want to.)");
+
+            _restarter = Console.ReadLine();
+
+            //For running again the problem computer.
+            if (_restarter == "y")
+            {
+                restarter = true;
+            }
+            else
+            {
+                restarter = false;
+            }
+
+        }
+
+    }
+}
---
+++ OOP Assignment 3/Program.cs
@@ -0,0 +1,40 @@
+﻿/****************************************************************************
+**					SAKARYA ÜNİVERSİTESİ
+**				BİLGİSAYAR VE BİLİŞİM BİLİMLERİ FAKÜLTESİ
+**				    BİLGİSAYAR MÜHENDİSLİĞİ BÖLÜMÜ
+**				   NESNEYE DAYALI PROGRAMLAMA DERSİ
+**					2014-2015 BAHAR DÖNEMİ
+**	
+**				ÖDEV NUMARASI..........: G181210017
+**				ÖĞRENCİ ADI............: SALİH EREN DEĞİRMENCİ
+**				ÖĞRENCİ NUMARASI.......: G181210017 
+**              DERSİN ALINDIĞI GRUP...: D
+****************************************************************************/
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ODEV_3_1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operator Opr_1 = new Operator();
+           
+            
+            //Run Calculate function
+            do
+            {
+                Opr_1.Calculate();
+                
+
+            } while (Opr_1.restarter);
+        }
+    }
+}
